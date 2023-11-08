3330ee9d57ef574f9806eaac777b8ce7c1fe7339
Artur Alves <arturalves.ca@gmail.com>
2023-11-08 14:37:29 +0100

    add comments

--- main.py
+++ main.py
@@ -1,77 +1,174 @@
 import json
 import ritoGomes as riot
-from dataclasses import dataclass
-from typing import List, Optional
+from typing import Optional, Union, List
 
-@dataclass
-class Champion:
-    name: str
-    kills: int = 0
-    deaths: int = 0
-    assists: int = 0
-    wins: int = 0
-    role: dict[str, int] = None # top, 10; adc, 20
+# Utility 
+def write_to_json(data : Union[dict, list], file_name : str) -> None:
+    with open(file_name, 'w') as json_file:
+        json.dump(data, json_file)
 
-@dataclass
-class Region:
-    name: str
-    champions: List[Champion] = None
-
+def read_json(file_name : str) -> Optional[dict]:
+    try:
+        with open(file_name, 'r') as json_file:
+            return json.load(json_file)
+    except FileNotFoundError:
+        return None
+    
 """
 Updates the match list containing the
 union of the $games_per_player last matches of every player in $elo from $region
+"""
+def update_matchid_list(region: str, elo: str, games_per_player: int = 20) -> None:
+    matchid_list = set()
+    league = riot.request_league(region, elo)
+    for i, item in enumerate(league['entries'], 1):
+        player = riot.request_player(region, item['summonerId'])
+        matchid_list.update(riot.request_match_list(region, player['puuid'], count = games_per_player))
+        print(i, item['summonerName'])
 
-e.g. 
-games_per_player: 20 
-region: euw1, br1
-elo: challenger, grandmaster, master
+    write_to_json(list(matchid_list), f'data/matchid_{region}_{elo}.json')
 
 """
-def update_match_list(region: str, elo: str, games_per_player: int = 20):
-    with open(f'data/matchid_{region}_{elo}.json', 'w') as json_file:
-        new_match_list = set()
-        league = riot.request_league(region, elo)
-        for i, item in enumerate(league['entries']):
-            player = riot.request_player(region, item['summonerId'])
-            new_match_list.update(riot.request_match_list(region, player['puuid'], count = games_per_player))
-            print(i, item['summonerName'])
+Updates heatmap data containing 
+a list of positions for every minute in the game of each champion
 
-        json.dump(list(new_match_list), json_file)
+[champion] -> [minute] -> {x, y}
 
+"""
+def update_heatmap(region: str, elo: str) -> None:
+    # update heatmap_data containing 
+    def update_heatmap_from_match(matchid: str, heatmap_data: dict) -> None:
 
-def update_heatmap_from_match(region : str, matchid : str, heatmap_data : dict):
-
-    match_data = riot.request_match(region, matchid)
-    part_champ = {str(i+1) : participant['championName'] for i, participant in enumerate(match_data['info']['participants'])}
-
-    timeline_data = riot.request_match_timelie(region, matchid)
-
-    for frame, frame_data in enumerate(timeline_data['info']['frames']):
-        for participant, champion in part_champ.items():
-            if champion not in heatmap_data.keys():
-                heatmap_data[champion] = dict()
-            if frame not in heatmap_data[champion].keys():
-                heatmap_data[champion][frame] = []
-                        
-            heatmap_data[champion][frame].append(frame_data['participantFrames'][participant]['position'])
-
-
-def update_heatmap(region : str, elo : str):
-    try:
-        with open(f'data/heatmap_{region}_{elo}.json', 'r') as json_file:
-            heatmap_data = json.load(json_file)
-    except FileNotFoundError:
+        champion_list = riot.request_champions_in_match(region, matchid)
+        timeline_data = riot.request_match_timelie(region, matchid)
+        
+        for frame_idx, frame_data in enumerate(timeline_data['info']['frames']):
+            for participant_idx, champion in enumerate(champion_list, 1):
+                if champion not in heatmap_data.keys():
+                    heatmap_data[champion] = dict()
+                if frame_idx not in heatmap_data[champion].keys():
+                    heatmap_data[champion][frame_idx] = []
+                heatmap_data[champion][frame_idx].append(frame_data['participantFrames'][str(participant_idx)]['position'])
+    
+    # read matchid data
+    if not (matchid_data := read_json(f'data/matchid_{region}_{elo}.json')):
+        print('matchid file not found')
+        return
+    
+    # read heatmap data
+    if not (heatmap_data := read_json(f'data/heatmap_{region}_{elo}.json')):
         heatmap_data = dict()
-
-    with open(f'data/matchid_{region}_{elo}.json', 'r') as json_file:
-        matchid_data = json.load(json_file)
-
-    for i, matchid in enumerate(matchid_data):
-        update_heatmap_from_match(region, matchid, heatmap_data)
+    
+    # update heatmap data using all matches
+    for i, matchid in enumerate(matchid_data, 1):
+        update_heatmap_from_match(matchid, heatmap_data)
         print(i, matchid)
 
-    with open(f'data/heatmap_{region}_{elo}.json', 'w') as json_file:
-        json.dump(heatmap_data, json_file)
+    write_to_json(heatmap_data, f'data/heatmap_{region}_{elo}.json')
 
-# update_match_list('euw1', 'challenger')
-update_heatmap('euw1', 'challenger')
+"""
+Uploads champion stats data containing
+a list of stat info for each champion
+
+[champion] -> List[{tons of data}]
+
+"""
+def update_champion_stats(region: str, elo: str, wanted_stats: List[str]) -> None:
+    # read matchid data
+    if not (matchid_data := read_json(f'data/matchid_{region}_{elo}.json')):
+        print('matchid file not found')
+        return
+    # read champion_stats data
+    # if not (stats_data := read_json(f'data/championstats_{region}_{elo}.json')):
+        # stats_data = dict()
+    stats_data = dict()
+
+    # update stats data using all matches
+    for i, matchid in enumerate(matchid_data, 1):
+        champion_list = riot.request_champions_in_match(region, matchid)
+        match_data = riot.request_match_data(region, matchid)
+
+        for participant_idx, champion in enumerate(champion_list):
+            if champion not in stats_data.keys():
+                stats_data[champion] = list()
+            
+            # filter data
+            champ_data = match_data['info']['participants'][participant_idx]
+            champ_data = {key:value for key, value in champ_data.items() if key in wanted_stats}
+            
+            stats_data[champion].append(champ_data)
+        print(i, matchid)
+
+    write_to_json(stats_data, f'data/championstats_{region}_{elo}.json')
+
+def update_ban_rate(region: str, elo: str) -> None:
+    # read matchid data
+    if not (matchid_data := read_json(f'data/matchid_{region}_{elo}.json')):
+        print('matchid file not found')
+        return
+    # ban data for each champ
+    ban_data = dict()
+
+    # champion id to champion name map
+    id_to_name = riot.request_champions_id() 
+    
+    for i, matchid in enumerate(matchid_data, 1):
+        match_data = riot.request_match_data(region, matchid)
+        ban_list = [id_to_name[ban['championId']] for ban in (match_data['info']['teams'][0]['bans'] + match_data['info']['teams'][1]['bans']) if ban['championId'] != -1]
+        
+        # count champion
+        for champion in ban_list:
+            if champion not in ban_data.keys():
+                ban_data[champion] = 0
+            ban_data[champion] += 1 / len(matchid_data)
+
+        print(i, matchid)
+
+    write_to_json(ban_data, f'data/championban_{region}_{elo}.json')
+
+
+def main():
+    update_heatmap('kr', 'challenger')
+
+if __name__ == "__main__":
+    main()
+
+
+# wanted_stats = ['win', 'kills', 'deaths', 'assists', 'goldEarned', 'neutralMinionsKilled', 'timePlayed', 'visionScore', 'totalHealsOnTeammates', 'totalDamageDealt', 'timeCCingOthers', 'totalDamageTaken', 'baronKills', 'dragonKills', 'inhibitorKills', 'turretKills']
+# combat_stats = ['kills', 'deaths', 'assists', 'timeCCingOthers', 'totalHealsOnTeammates', 'totalDamakeTaken', 'totalDamageDealt']
+# income_control_stats = ['goldEarned', 'neutralMinionsKilled', 'timePlayed', 'visionScore', 'baronKills', 'dragonKills', 'turretKills', 'inhibitorKills']
+# wanted_stats = combat_stats + income_control_stats
+# update_champion_stats('euw1', 'challenger', wanted_stats = wanted_stats)
+# update_ban_rate('br1', 'challenger')
+
+# https://mobalytics.gg/blog/how-to-use-the-league-of-legends-stats-tab-to-improve/
+# here we can find ideas for stats separated in combat / income / map control
+
+# CHAMP LIST: (allow sorting by)
+# --> Times played (prob. rate)
+# --> game time
+# --> total gold
+# --> pick rate
+# --> ban rate 
+# --> win rate
+# --> kda
+# --> cs / min
+
+# RADAR CHART: 
+# 1. Combat:
+#   --> kda ratio (kills + assists / deaths)
+#   --> kill participation (kills + assists / total kills)
+#   --> utility score (crowd control, team healing and team dmg red)
+#   --> damage per death (total damage / deaths)
+#   --> damage share (total damage / team total damage)
+
+
+# 2. Income & Control:
+#   --> damage per gold (total damage / total gold)
+#   --> cs per minute
+#   --> vision score
+#   --> objectives (dragonKills, baronKills, turretKills, inhibitorKills)
+
+# hard ...
+#   --> early gold adv (gold at 15 min / oponent's gold at 15 min)
+#   --> early cs adv (cs at 15 / oponent's cs at 15 min)
--- ritoGomes.py
+++ ritoGomes.py
@@ -3,9 +3,12 @@ import os
 import functools
 from ratelimit import limits, sleep_and_retry
 
-# key is reset every 24h 
+# key must be reset every 24h 
 # request limit: 20/s or 100/min
 api_key = os.getenv('API_KEY')
+if api_key is None:
+    raise Exception("Provide API_KEY")
+default_patch = "13.19.1"
 
 # decorator to verify get request response
 def verify_response(func):
@@ -13,7 +16,8 @@ def verify_response(func):
     def wrapper(*args, **kwargs):
         response = func(*args, **kwargs)
         if not response.ok: 
-            raise Exception("Response > 200")
+            print(response)
+            raise Exception(f"Error: {response.status_code}")
         return response
     return wrapper
 
@@ -21,51 +25,90 @@ def verify_response(func):
 requests.get = verify_response(requests.get)
 requests.get = limits(calls = 100, period = 150)(requests.get)
 requests.get = sleep_and_retry(requests.get)
-# requests.get = sleep_and_retry(limits(calls = 100, period = 150)(requests.get))
-
 
 map_region = {
     "euw1" : "europe",
-    "br1" : "americas", 
+    "eun1" : "europe",
+    "br1" : "americas",
+    "na1" :"americas",
+    "kr" : "asia",
+    "jp1" : "asia",
+    "oc1" : "sea",
 }
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ API requests ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+"""
+Return player information of $region from summonerId (def.) or name
 
-def request_player(region: str, summoner_name: str, byName: bool = False) -> dict:
+use case: capture player's 'puuid' 
+"""
+def request_player(region: str, summoner: str, byName: bool = False) -> dict:
     url="lol/summoner/v4/summoners"
 
     if byName:
         url += "/by-name"
     
-    response = requests.get(
-        f"https://{region}.api.riotgames.com/{url}/{summoner_name}",
+    return requests.get(
+        f"https://{region}.api.riotgames.com/{url}/{summoner}",
         params = {"api_key": api_key}
-    )
+    ).json()
 
-    return response.json()
 
+"""
+Return informations about the players from $region in $elo
+
+use case: get summonerIds
+"""
 def request_league(region: str, elo: str, queue: str = "RANKED_SOLO_5x5") -> dict: 
     return requests.get(
         f"https://{region}.api.riotgames.com/lol/league/v4/{elo}leagues/by-queue/{queue}",
         params = {"api_key": api_key}
     ).json()
 
-# regions available for match information: americas, europe, asia, sea
+"""
+Return $count matches starting at $start index for player with $puuid from $region
+
+use case: get list of match ids
+"""
 def request_match_list(region: str, puuid: str, count: int = 20, start: int = 0) -> dict:
+    # regions available for match information: americas, europe, asia, sea
     return requests.get(
         f"https://{map_region[region]}.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids",
         params = {"type": "ranked", "start": str(start), "count": str(count), "api_key": api_key}
     ).json()
 
-def request_match(region: str, matchid: str) -> dict:
+"""
+Return general match information from its $matchid and $region
+"""
+def request_match_data(region: str, matchid: str) -> dict:
     return requests.get(
         f"https://{map_region[region]}.api.riotgames.com/lol/match/v5/matches/{matchid}",
         params = {"api_key": api_key}
     ).json()
 
+"""
+Return detailed mach information for every minute in the game from its $match and $region
+"""
 def request_match_timelie(region: str, matchid: str) -> dict:
     return requests.get(
         f"https://{map_region[region]}.api.riotgames.com/lol/match/v5/matches/{matchid}/timeline",
         params = {"api_key": api_key}
     ).json()
 
+"""
+Return dict {paticipant_idx : champion} for match from its $region and $matchid
+
+*participant_idx is 1-idx and in str format
+"""
+def request_champions_in_match(region: str, matchid: str) -> list:
+    match_data = request_match_data(region, matchid)
+    champion_list = [participant['championName'] for participant in match_data['info']['participants']]
+    return champion_list
+
+def request_champions_id(patch = default_patch) -> dict:
+    champion_data = requests.get(f"http://ddragon.leagueoflegends.com/cdn/{patch}/data/en_US/champion.json").json()
+    id_to_name = dict()
+    for champion in champion_data['data'].values():
+        # champion['key'] is id  (e.g. 266) and champion['id'] is name (e.g. Aatrox)
+        id_to_name[int(champion['key'])] = champion['id']
+    return id_to_name
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
