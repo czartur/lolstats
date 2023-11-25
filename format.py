7ddf353b97ba9550e15c58ffaed288879e55cb9a
Artur Alves <arturalves.ca@gmail.com>
2023-11-25 23:18:39 +0100

    data extraction scripts

---
+++ format.py
@@ -0,0 +1,91 @@
+import json
+import os
+
+regions = ["euw1", "eun1", "tr1", "ru", "br1", "na1", "la1", "la2", "kr", "jp1", "oc1", "ph2", "sg2", "th2", "tw2", "vn2"]
+
+def FormatStats(region, elo):
+    
+    ban_path = f"data/championban_{region}_{elo}.json"
+    data_path = f"data/championstats_{region}_{elo}.json"
+    write_path = f"data/formated/stats_{region}_{elo}.json"
+
+    with open(data_path, 'r') as json_file:
+        data = json.load(json_file)
+    
+    with open(ban_path, 'r') as json_file:
+        ban = json.load(json_file)
+
+    attributes = {champion : {} for champion  in data}
+    maxes = {}
+    
+    # main data
+    for champion in data:
+        total = len(data[champion])
+        championStats = {}
+        for match in data[champion]: # array
+            # add kda
+            match['kda'] = (match['kills'] + match['assists']) / max(1, match['deaths'])
+
+            # add dmg per gold
+            match['damagePerGold'] = match['totalDamageDealt'] / max(1, match['goldEarned']) 
+            
+            # add (created) structure score
+            match['structureScore'] = (5*match['nexusTakedowns'] + 3*match['inhibitorTakedowns'] + 2*match['turretTakedowns'])
+
+            for stat in match:
+                if not isinstance(match[stat], (int,float)): continue 
+                if not stat in championStats:
+                    championStats[stat] = 0
+                championStats[stat] += match[stat] / total
+
+        # add popularity
+        championStats['popularity'] = len(data[champion])
+
+        # add ban
+        championStats['ban'] = ban[champion] if champion in ban.keys() else 0
+        
+        attributes[champion] = championStats
+    
+    for champion in data:
+        for stat in attributes[champion]:
+            if not stat in maxes:
+                maxes[stat] = 0
+            maxes[stat] = max(maxes[stat], attributes[champion][stat])
+    
+    for champion in data:
+        for stat in attributes[champion]:
+            attributes[champion][stat] = {
+                    'norm' : attributes[champion][stat] / maxes[stat] if maxes[stat] != 0 else attributes[champion][stat],
+                    'abs' : attributes[champion][stat]}
+    
+    with open(write_path, 'w') as json_file:
+        json.dump(attributes, json_file, indent=2)
+
+
+def generateChampionList(region, elo):
+    read_path = f"data/championstats_{region}_{elo}.json"
+    write_path = f"data/championlist.json"
+
+    with open(read_path, 'r') as json_file:
+        data = json.load(json_file)
+
+    if os.path.exists(write_path):
+        with open(write_path, 'r') as json_file:
+            championList = json.load(json_file)
+    else: 
+            championList = []
+
+    
+    championList = set(championList)
+
+    for champion in data:
+        championList.add(champion)
+
+    championList = list(championList)
+    with open(write_path, 'w') as json_file:
+        json.dump(championList, json_file, indent=2)
+
+
+generateChampionList('br1', 'challenger')
+# for region in regions:
+    # FormatStats(region, 'challenger')
